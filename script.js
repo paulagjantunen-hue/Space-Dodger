1dabbc6081d82d126391ce08bf6b1dc6dcc4cec5
Bjarke Hammerbak Paluszewski <bjarkehp@gmail.com>
2026-05-23 23:42:48 +0200

    implemented deltaTime into the game

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const scoreDisplay = document.getElementById("score");
 
+const scorePerSecond = 100;
+const asteroidsPerSecond = 5;
+const asteroidBaseSpeed = 150;
+const asteroidSpeedPerPoint = 1 / 10;
+
+let prevTime = 0;
+let currentTime = performance.now() / 1000
+let realDeltaTime = 0
+let deltaTime = 0
+let nextAsteroidTime = 0;
+
 const player = {
     x: 225,
     y: 620,
     width: 50,
     height: 50,
-    speed: 7
+    speed: 400
 };
 
 let keys = {};
 let asteroids = [];
 let score = 0;
 let gameOver = false;
-let asteroidSpeed = 3;
-let lastTime = performance.now();
+let asteroidSpeed = asteroidBaseSpeed;
 
 // Controls
 window.addEventListener("keydown", (e) => {
@@ -39,16 +49,17 @@ function drawPlayer() {
 
 function movePlayer() {
     if ((keys["ArrowLeft"] || keys["a"]) && player.x > 0) {
-        player.x -= player.speed;
+        player.x -= player.speed * deltaTime;
     }
 
     if ((keys["ArrowRight"] || keys["d"]) && player.x < canvas.width - player.width) {
-        player.x += player.speed;
+        player.x += player.speed * deltaTime;
     }
 }
 
 function createAsteroid() {
     const size = Math.random() * 40 + 20;
+    nextAsteroidTime += -Math.log(Math.random());
 
     asteroids.push({
         x: Math.random() * (canvas.width - size),
@@ -62,21 +73,21 @@ function drawAsteroids() {
     ctx.fillStyle = "gray";
 
     asteroids.forEach((asteroid) => {
-     ctx.beginPath();
-     ctx.arc(
-         asteroid.x + asteroid.width / 2,
-         asteroid.y + asteroid.height / 2,
-         asteroid.width / 2,
-         0,
-         Math.PI * 2
-     );
-     ctx.fill();
+        ctx.beginPath();
+        ctx.arc(
+            asteroid.x + asteroid.width / 2,
+            asteroid.y + asteroid.height / 2,
+            asteroid.width / 2,
+            0,
+            Math.PI * 2
+        );
+        ctx.fill();
     });
 }
 
 function moveAsteroids() {
     asteroids.forEach((asteroid) => {
-        asteroid.y += asteroidSpeed;
+        asteroid.y += asteroidSpeed * deltaTime;
     });
 
     asteroids = asteroids.filter((asteroid) => asteroid.y < canvas.height + 50);
@@ -105,21 +116,41 @@ function drawGameOver() {
 }
 
 function updateScore() {
-    score++;
+    score = Math.floor(currentTime * scorePerSecond);
     scoreDisplay.textContent = `Score: ${score}`;
+    asteroidSpeed = asteroidBaseSpeed + score * asteroidSpeedPerPoint;
+}
 
-    if (score % 500 === 0) {
-        asteroidSpeed += 0.5;
+function lerp(a, b, t) {
+    return a + (b - a) * t;
+}
+
+function updateTime() {
+    prevTime = currentTime;
+    currentTime = performance.now() / 1000;
+    realDeltaTime = currentTime - prevTime;
+
+    // Some browsers do not provide high precision time,
+    // as that can be used for fingerprinting.
+    // This smoothens out deltaTime, as to not cause jittery movement.
+    if (deltaTime > 0) {
+        deltaTime = lerp(deltaTime, realDeltaTime, 0.1);
+    } else {
+        deltaTime = realDeltaTime;
+    }  
+
+    // Time will keep going if the page is inactive, 
+    // but no animation frame will be given.
+    // This code limits currentTime and deltaTime to 1 second.
+    if (deltaTime > 1) {
+        deltaTime = 1;
+        currentTime = prevTime + 1;
     }
 }
 
-function gameLoop(currentTime) {
-    const deltaTime = (currentTime - lastTime) / 1000;
-    lastTime = currentTime;
-
-    player.x += playerSpeed * deltaTime;
+function gameLoop() {
+    updateTime();
     
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     if (gameOver) {
@@ -130,7 +161,7 @@ function gameLoop(currentTime) {
     movePlayer();
     moveAsteroids();
 
-    if (Math.random() < 0.03) {
+    while (currentTime >= nextAsteroidTime) {
         createAsteroid();
     }
 
@@ -143,4 +174,4 @@ function gameLoop(currentTime) {
     requestAnimationFrame(gameLoop);
 }
 
-requestAnimationFrame (gameLoop);
+requestAnimationFrame(gameLoop);
